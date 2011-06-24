01e09e5a861f1296cd1e512c2439ed14336d8c2a
kaorun55 <kaorun55@gmail.com>
2011-06-24 10:55:15 +0900

    コメントを振った

--- kinect_cpp_wrapper/include/kinect/nui/ImageFrame.h
+++ kinect_cpp_wrapper/include/kinect/nui/ImageFrame.h
@@ -12,6 +12,7 @@ namespace kinect {
 	namespace nui {
         class ImageStream;
 
+		/// 1フレームのデータ
         class ImageFrame
         {
         public:
@@ -19,20 +20,27 @@ namespace kinect {
             ImageFrame( ImageStream& imageStream, DWORD dwMillisecondsToWait = 0 );
             virtual ~ImageFrame();
 
+			/// 1ラインのバイト数を取得する
             INT Pitch(){ return lockedRect_.Pitch; };
+
+			/// データの先頭アドレスを取得する
             void* Bits() { return lockedRect_.pBits; };
 
+			/// 幅(解像度)を取得する
             UINT Width() const;
+			
+			/// 高さ(解像度)を取得する
             UINT Height() const;
 
         protected:
 
             ImageStream& imageStream_;
 
-            const NUI_IMAGE_FRAME *imageFrame_;
-            KINECT_LOCKED_RECT lockedRect_;
+            const NUI_IMAGE_FRAME *imageFrame_;	///< フレームデータ
+            KINECT_LOCKED_RECT lockedRect_;		///< 矩形データ
         };
 
+		/// カメラ画像の1フレームデータ
         class VideoFrame : public ImageFrame
         {
         public:
@@ -40,9 +48,11 @@ namespace kinect {
             VideoFrame( ImageStream& imageStream, DWORD dwMillisecondsToWait = 0 );
             virtual ~VideoFrame();
 
+			// 指定座標のデータ
             UINT operator () ( UINT x, UINT y );
         };
 
+		/// 距離画像の1フレームデータ
         class DepthFrame : public ImageFrame
         {
         public:
@@ -50,6 +60,7 @@ namespace kinect {
             DepthFrame( ImageStream& imageStream, DWORD dwMillisecondsToWait = 0 );
             virtual ~DepthFrame();
 
+			// 指定座標のデータ
             USHORT operator () ( UINT x, UINT y );
         };
     }
