c0ef694369fded9f0c69788bda7e029069e62481
kaorun55 <kaorun55@gmail.com>
2011-06-30 13:16:02 +0900

    C#版のGetKinectAudioInfoサンプルを作成

---
+++ kinect_sdk_samples/kinect_sdk_samples_cs/audio_device_info/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Research.Kinect.Audio;
+
+namespace audio_device_info
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            KinectAudioSource kas = new KinectAudioSource();
+            foreach ( AudioDeviceInfo device in kas.FindCaptureDevices() ) {
+                Console.WriteLine( "DeviceIndex:" + device.DeviceIndex + " " +
+                                   "DeviceName :" + device.DeviceName + " " +
+                                   "DeviceID   :" + device.DeviceID );
+            }
+        }
+    }
+}
