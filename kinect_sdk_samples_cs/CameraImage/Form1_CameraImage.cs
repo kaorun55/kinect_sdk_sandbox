5340866baabf9a9685dc1119a94103ded543adca
kaorun55 <kaorun55@gmail.com>
2011-09-13 16:48:11 +0900

    C#版MS SDKのデプスを追加(未動作確認)

--- kinect_sdk_samples_cs/CameraImage/Form1_CameraImage.cs
+++ kinect_sdk_samples_cs/CameraImage/Form1_CameraImage.cs
@@ -10,32 +10,31 @@ namespace CameraImage
     // アプリケーション固有の処理を記述
     partial class Form1
     {
-        private Runtime runtime;
+        private Runtime runtime = new Runtime();
 
         // 初期化
         private void xnInitialize()
         {
-            // コンテキストの初期化 ... (1)
-            runtime = new Runtime();
+            // ランタイムの初期化 ... (1)
             runtime.Initialize( RuntimeOptions.UseColor );
 
-            // イメージジェネレータの作成 ... (2)
+            // ビデオ、デプスストリームの作成
             runtime.VideoStream.Open( ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color );
         }
 
         // 描画
         private void xnDraw()
         {
-            // カメライメージの更新を待ち、画像データを取得する ... (4)
-            var image = runtime.VideoStream.GetNextFrame( 0 );
+            // ビデオイメージの更新を待ち、画像データを取得する ... (4)
+            var video = runtime.VideoStream.GetNextFrame( 0 );
 
-            // カメラ画像の表示 ... (5)
+            // 画像の作成 ... (5)
             lock ( this ) {
                 // 書き込み用のビットマップデータを作成
                 Rectangle rect = new Rectangle( 0, 0, bitmap.Width, bitmap.Height );
                 BitmapData data = bitmap.LockBits( rect, ImageLockMode.WriteOnly,
                                       System.Drawing.Imaging.PixelFormat.Format24bppRgb );
-                Marshal.Copy( image.Image.Bits, 0, data.Scan0, image.Image.Bits.Length );
+                Marshal.Copy( video.Image.Bits, 0, data.Scan0, video.Image.Bits.Length );
                 bitmap.UnlockBits( data );
             }
         }
---
+++ kinect_sdk_samples_cs/Depth/Form1_Depth.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Microsoft.Research.Kinect.Nui;
+using System.Runtime.InteropServices;
+
+namespace Depth
+{
+    // アプリケーション固有の処理を記述
+    partial class Form1
+    {
+        private Runtime runtime;
+
+        // 描画に必要なオブジェクト
+        private Brush brush = new SolidBrush( Color.Black );
+        private Font font = new Font( "ＭＳ ゴシック", 30 );
+
+        // 初期化
+        private void xnInitialize()
+        {
+            // ランタイムの初期化
+            runtime.Initialize( RuntimeOptions.UseColor | RuntimeOptions.UseDepth );
+
+            // ビデオ、デプスストリームの作成
+            runtime.VideoStream.Open( ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color );
+            runtime.DepthStream.Open( ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth );
+        }
+
+        // 描画
+        private unsafe void xnDraw()
+        {
+            // ビデオ、デプスの更新を待ち、データを取得する
+            var video = runtime.VideoStream.GetNextFrame( 0 );
+            var depth = runtime.DepthStream.GetNextFrame( 0 );
+
+            // 画像の作成
+            lock ( this ) {
+                // 書き込み用のビットマップデータを作成
+                Rectangle rect = new Rectangle( 0, 0, bitmap.Width, bitmap.Height );
+                BitmapData data = bitmap.LockBits( rect, ImageLockMode.WriteOnly,
+                                    System.Drawing.Imaging.PixelFormat.Format24bppRgb );
+                Marshal.Copy( video.Image.Bits, 0, data.Scan0, video.Image.Bits.Length );
+                bitmap.UnlockBits( data );
+
+                // 中心点の距離を表示
+                Graphics g = Graphics.FromImage( bitmap );
+
+                int x = (int)video.Image.Width / 2;
+                int y = (int)video.Image.Height / 2;
+                g.FillEllipse( brush, x - 10, y - 10, 20, 20 );
+
+                // depthの中心点を取る
+                string message = /*depth.Image.Bits[x, y] +*/ "mm";
+                g.DrawString( message, font, brush, x, y );
+            }
+        }
+
+        // キーイベント
+        private void xnKeyDown( Keys key )
+        {
+        }
+    }
+}
