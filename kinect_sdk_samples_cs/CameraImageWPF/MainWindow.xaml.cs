9cd6eb8da18383ee90076334ad56031149268c7a
kaorun55 <kaorun55@gmail.com>
2011-12-05 01:03:49 +0900

    WPFのサンプルを追加

---
+++ kinect_sdk_samples_cs/CameraImageWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using Microsoft.Research.Kinect.Nui;
+
+namespace CameraImageWPF
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Runtime kinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kinect = Runtime.Kinects[0];
+            kinect.Initialize( RuntimeOptions.UseColor );
+            kinect.VideoStream.Open( ImageStreamType.Video, 2,
+                ImageResolution.Resolution640x480, ImageType.Color );
+            kinect.VideoFrameReady +=
+                new EventHandler<ImageFrameReadyEventArgs>( kinect_VideoFrameReady );
+        }
+
+        void kinect_VideoFrameReady( object sender, ImageFrameReadyEventArgs e )
+        {
+            PlanarImage image = e.ImageFrame.Image;
+            kinectImage.Source = BitmapSource.Create( image.Width, image.Height, 96, 96,
+                PixelFormats.Bgr32, null, image.Bits, image.Width * image.BytesPerPixel );
+        }
+
+    }
+}
