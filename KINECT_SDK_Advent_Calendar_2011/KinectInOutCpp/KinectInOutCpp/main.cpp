f7b42f2cb8f5e5d19ef5d6b06186f5c117feba1c
kaorun <kaorun55@gmail.com>
2011-12-22 01:34:04 +0900

    C++挿抜サンプル追加:

---
+++ KINECT_SDK_Advent_Calendar_2011/KinectInOutCpp/KinectInOutCpp/main.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+#include <MSR_NuiApi.h>
+
+int GetDeviceCount()
+{
+	int count = 0;
+	::MSR_NUIGetDeviceCount( &count );
+	return count;
+}
+
+void CALLBACK StatusProc( const NuiStatusData* data )
+{
+	std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
+	std::wcout << "Name : " << data->instanceName <<
+		          " Status : " << (SUCCEEDED( data->hrStatus ) ? " connect" : " disconnect") << std::endl;
+}
+
+void main()
+{
+	std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
+	::MSR_NuiSetDeviceStatusCallback( &::StatusProc );
+
+	while ( ::_kbhit() == 0 ) {
+		::Sleep( 100 );
+	}
+}
