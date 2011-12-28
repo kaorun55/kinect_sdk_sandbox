50e04d0aceacaca2cc7a571766a50a49c44f3b7f
kaorun <kaorun55@gmail.com>
2011-12-28 12:14:32 +0900

    音声のサンプルを追加

---
+++ KINECT_SDK_Advent_Calendar_2011/Audio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Research.Kinect.Audio;
+
+namespace Audio
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            // 音源を取得するインスタンスを生成する
+            using ( KinectAudioSource source = new KinectAudioSource() ) {
+                // SingleChannelAec:シングルチャネルのマイクで、エコーキャンセルを使用する
+                // OptibeamArrayOnly:マルチチャネルのマイクのみを使用する(エコーキャンセルを使用しない)
+                // OptibeamArrayAndAec:マルチチャネルのマイクと、エコーキャンセルを使用する)
+                // SingleChannelNsAgc:???
+                source.SystemMode = SystemMode.SingleChannelAec;
+
+                using ( Stream audioStream = source.Start() ) {
+                    Console.WriteLine( "Start... Press any key" );
+
+                    byte[] buffer = new byte[4096];
+                    Win32.StreamingWavePlayer player = new Win32.StreamingWavePlayer( 16000, 16, 1, 100 );
+                    while ( !Console.KeyAvailable ) {
+                        int count = audioStream.Read( buffer, 0, buffer.Length );
+                        player.Output( buffer );
+                    }
+                }
+            }
+        }
+    }
+}
--- KINECT_SDK_Advent_Calendar_2011/KinectInOutCpp/KinectInOutCpp/main.cpp
+++ KINECT_SDK_Advent_Calendar_2011/KinectInOutCpp/KinectInOutCpp/main.cpp
@@ -5,24 +5,24 @@
 
 int GetDeviceCount()
 {
-	int count = 0;
-	::MSR_NUIGetDeviceCount( &count );
-	return count;
+    int count = 0;
+    ::MSR_NUIGetDeviceCount( &count );
+    return count;
 }
 
 void CALLBACK StatusProc( const NuiStatusData* data )
 {
-	std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
-	std::wcout << "Name : " << data->instanceName <<
-		          " Status : " << (SUCCEEDED( data->hrStatus ) ? " connect" : " disconnect") << std::endl;
+    std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
+    std::wcout << "Name : " << data->instanceName <<
+                  " Status : " << (SUCCEEDED( data->hrStatus ) ? " connect" : " disconnect") << std::endl;
 }
 
 void main()
 {
-	std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
-	::MSR_NuiSetDeviceStatusCallback( &::StatusProc );
+    std::cout << GetDeviceCount() << " KINECT(s)" << std::endl;
+    ::MSR_NuiSetDeviceStatusCallback( &::StatusProc );
 
-	while ( ::_kbhit() == 0 ) {
-		::Sleep( 100 );
-	}
+    while ( ::_kbhit() == 0 ) {
+        ::Sleep( 100 );
+    }
 }
