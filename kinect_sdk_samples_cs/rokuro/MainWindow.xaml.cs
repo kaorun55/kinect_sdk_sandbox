d9d0fe7515050e86a014fbc3228189408d190bbc
kaorun55 <kaorun55@gmail.com>
2012-03-19 15:36:08 +0900

    T-pot を追加

---
+++ kinect_sdk_samples_cs/rokuro/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoDev;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Trackball _trackball;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _trackball = new Trackball();
+            _trackball.Attach( this );
+            _trackball.Slaves.Add( myViewport3D );
+            _trackball.Enabled = true;
+        }
+
+        protected override void OnKeyDown( KeyEventArgs e )
+        {
+            base.OnKeyDown( e );
+            if ( e.Key == Key.Escape )
+                this.Close();
+        }
+    }
+}
